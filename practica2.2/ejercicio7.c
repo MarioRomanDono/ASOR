3d4af954acbcb4825026da18789286a02c66c661
Mario Román Dono <mario.romandono@gmail.com>
2021-11-08 09:20:34 +0100

    Ejercicio 7

---
+++ practica2.2/ejercicio7.c
@@ -0,0 +1,12 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(void) {
+	umask(0027);
+	int fd = open("prueba", O_RDWR | O_CREAT, 0645);
+	if (fd == -1) return -1;
+	close(fd);
+	return 0;
+}
