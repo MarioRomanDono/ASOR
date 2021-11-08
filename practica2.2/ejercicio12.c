aa3a785adca672c5e098836e38d8a8073ea2dcc4
Mario Román Dono <mario.romandono@gmail.com>
2021-11-08 10:16:30 +0100

    Ejercicio 12

---
+++ practica2.2/ejercicio12.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+int main(int argc, char ** argv) {
+	int fd;
+
+	if (argc != 2) {
+		fprintf(stderr, "Usage: ./mydup <file>\n");
+		return -1;
+	}
+	
+	if ((fd = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0644)) == -1) {
+		perror("Error while opening file");
+		return -1;
+	}
+
+	if (dup2(fd, 1) == -1)  {
+		perror("Error while executing dup2");
+		return -1;
+	}
+	
+	printf("Esto es una prueba de que la salida estándar se redirige\n");
+	close(fd);
+	return 0;
+}
